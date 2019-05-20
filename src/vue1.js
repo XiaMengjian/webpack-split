5c85f3acf3433a22de7eab1f922297fa39690502
xia <xiamengjian47@gmail.com>
2019-05-20 22:33:57 +0800

    测试环节

--- src/vue1.js
+++ src/vue1.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
-import _ from 'lodash'
-import $ from './assets/jquery'
+// import _ from 'lodash'
+// import $ from './assets/jquery'
 import Echarts from './assets/echarts'
-
-console.log('vue2  3 xxx ggggg 反反复复 ttt  frrr ffff')
 
 new Vue({
     el: '#app',
--- src/vue2.js
+++ src/vue2.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import $ from './assets/jquery'
 import html2Canvas from 'html2canvas'
+import Echarts from './assets/echarts'
 
 new Vue({
     el: '#app',
-    template: '<div><h1>我是Vue2</h1><Temp2></Temp2>/div>'
+    template: '<div><h1>我是Vue2</h1>/div>'
 });
